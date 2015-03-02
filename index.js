1603e3fd5dea4291d5a3c9c19719f8521768286c
Reid Carlberg <reid.carlberg@reidcarlbe-ltm.internal.salesforce.com>
2015-03-02 06:02:42 -0600

    initial

--- index.js
+++ index.js
@@ -10,11 +10,15 @@ var express = require('express'),
 	bodyParser = require('body-parser'),
 	Sequelize = require('sequelize');
 
+var dbUrl = process.env.HEROKU_POSTGRESQL_AQUA_URL || 'postgres://reid.carlberg@127.0.0.1:5432/ormtest1';
+
+console.log("Selected DB: " + dbUrl);
+
 var app = express();
 
-var sequelize = new Sequelize('postgres://reid.carlberg@127.0.0.1:5432/ormtest1');
+var sequelize = new Sequelize(dbUrl);
 
-var permitted = ["73.208.78.180", "127.0.0.1", "192.168.1.23", "192.168.1.92"]
+var permitted = ["73.208.78.180", "127.0.0.1", "192.168.1.250", "192.168.1.23", "192.168.1.92"]
 
 //http://stackoverflow.com/questions/7185074/heroku-nodejs-http-to-https-ssl-forced-redirect/23894573#23894573
 var forceSsl = function (req, res, next) {
@@ -53,41 +57,18 @@ app.get('/', function(request, response) {
 app.post('/report', function(request, response) {
 
 	var responseStatus = null;
-	//get request
-	console.log(request.body);
 
-	//ensure we received data
-	var data = request.body;
-	if (!data) {
-		console.log("null body error");
-		responseStatus = 400;
-	}
-	if (!data.deviceId || !data.reportTime || !data.averageBrightness || !data.lowBrightness || !data.highBrightness || !data.lowTimestamp || !data.highTimestamp) {
-		console.log("data missing error");
-		responseStatus = 400;
-	}
+	var newReadings = request.body;
 
-	//are the timestamps timestamps?
-	if (!Date.parse(data.reportTime) || !Date.parse(data.lowTimestamp) || !Date.parse(data.highTimestamp)) {
-		console.log("timestamp error");
-		responseStatus = 400;
-	}
-
-	if (!responseStatus) {
-		//normalize the readings
-		data.highBrightness = utility.normalize(data.highBrightness);
-		data.lowBrightness = utility.normalize(data.lowBrightness);
-		data.averageBrightness = utility.normalize(data.averageBrightness);
-
-		//store the data
-		try {
-			storage.store(data);
-			responseStatus = 200;
-		} catch (err) {
-			console.log(err);
-			responseStatus = 500;
+	sequelize.sync().then(function() {
+		var myKeys = Object.keys(newReadings);
+		for (i = 0; i < myKeys.length; i++) {
+			var currentReading = newReadings[myKeys[i]];
+			Reading.create(currentReading);
 		}
-	}
+	}).then(function(data) {
+		console.log("done");
+	});
 
 	//send response
 	response.sendStatus(responseStatus);
